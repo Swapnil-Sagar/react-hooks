90b12bd18f89208b3871cabf509e1c23604bf09b
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-01-27 09:48:48 +0530

    useEffect with incorrect dependency

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import HookCounter2 from "./components/HookCounter2";
 import HookCounter3 from "./components/HookCounter3";
 import HookCounter4 from "./components/HookCounter4";
 import HookMouse from "./components/HookMouse";
+import IntervalHookCounter from "./components/IntervalHookCounter";
 import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
-      {/* <HookMouse />
+      <IntervalHookCounter />
+      {/* <MouseContainer/>
+      <HookMouse />
       <HookCounter1 />
       <ClassCounterOne />
       <ClassCounter />
