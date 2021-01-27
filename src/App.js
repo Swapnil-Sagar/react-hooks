61642e6ec8eaff61d91a6bf8da132279994502e2
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-01-27 07:15:30 +0530

    use state with object

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounter2 from "./components/HookCounter2";
+import HookCounter3 from "./components/HookCounter3";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ClassCounter />
       <HookCounter />
       <HookCounter2 />
+      <HookCounter3 />
     </div>
   );
 }
